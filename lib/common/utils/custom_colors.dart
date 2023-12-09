1e015e9ab7ef257ce19dd30dcec77560d1d4b2e1
kinannwidya <87623450+kinannwidya@users.noreply.github.com>
2023-12-09 21:26:59 +0700

    Create custom_colors.dart

---
+++ lib/common/utils/custom_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Coloors{
+  Coloors._();
+
+  static const Color primary = Color(0xFF00880D);
+  static const Color second = Color(0xFFFFFFFF);
+  static const Color notif = Color(0xFFBC0404);
+
+}
