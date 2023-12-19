847ec5ac415ba723eb133be91915f0abfc6bd515
fauzanylfd <119656350+fauzanylfd@users.noreply.github.com>
2023-12-19 14:24:02 +0700

    Add files via upload

---
+++ lib/component/foodcart.dart
@@ -0,0 +1,42 @@
+import 'package:gopet/component/serviceModel.dart';
+
+class foodcart{
+  final String image;
+  final String name;
+  final double price;  // Ubah tipe data menjadi double
+  final double discountPrice;  // Ubah tipe data menjadi double
+  bool isFavorite;
+
+  foodcart({
+    required this.image,
+    required this.name,
+    required this.price,
+    required this.discountPrice,
+    this.isFavorite = false, 
+  });
+ set favorite(bool value) {
+    isFavorite = value;
+  }
+}
+
+List<foodcart> Foodscart = [
+  foodcart(
+    image:"assets/images/Rectangle 45.png" ,
+    name: 'Makanan Hewan A',
+    price: 50000.0,  // Ubah nilai menjadi double
+    discountPrice: 45000.0,  // Ubah nilai menjadi double
+  ),
+  foodcart(
+    image:"assets/images/Rectangle 49.png" ,
+    name: 'Makanan Hewan B',
+    price: 60000.0,  // Ubah nilai menjadi double
+    discountPrice: 55000.0,  // Ubah nilai menjadi double
+  ),
+  foodcart(
+    image:"assets/images/Rectangle 52.png" ,
+    name: 'Makanan Hewan C',
+    price: 70000.0,  // Ubah nilai menjadi double
+    discountPrice: 65000.0,  // Ubah nilai menjadi double
+  ),
+];
+
